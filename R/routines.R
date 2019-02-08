4623e9f91c2c0a169d0f5a60a98900d51b32ba45
Luquette <ljl11@compute-a-16-127.o2.rc.hms.harvard.edu>
2019-02-07 20:12:40 -0500

    Add basic joint caller

--- R/routines.R
+++ R/routines.R
@@ -576,3 +576,72 @@ plot.gp.confidence <- function(pos, gp.mu, gp.sd, df, sd.mult=2,
         col=tube.col, border=line.col, lty=tube.lty)
     lines(pos, gp.mu, lwd=2, col=line.col, lty=line.lty)
 }
+
+
+
+#############################################################################
+# routines for joint calling
+#############################################################################
+
+joint.caller <- function(dfs, hdfs) {
+    jdf <- jhelper(dfs)
+    jhdf <- jhelper(hdfs)
+    cutoffs <- get.joint.cutoffs(jhdf, n=length(dfs))
+    jdf$pass <- apply.joint.cutoffs(jdf, cutoffs)
+    dfs <- lapply(dfs, function(df) {
+        # af > 1/dp is a roundabout way to enforce alt>1, because
+        # at this point in the pipeline the column corresponding to
+        # alt counts for this sample has been lost.
+        df$jpass <- as.logical(jdf$pass * (df$af > 0) &
+            df$dp.test & df$lowmq.test & df$af > 1/df$dp)
+        df$jabc <- jdf$jabc
+        df
+    })
+    dfs
+}
+
+
+
+jhelper <- function(dfs, gp.sd.penalty=0.1, gp.sd.cutoff=1) {
+    n=length(dfs)
+    cps <- 4 # columns per sample
+    jdf <- do.call(cbind, lapply(dfs,function(df) df[,c('af','dp','abc.pv','gp.sd')]))
+
+    jdf$ncells <- rowSums(!apply(jdf[,seq(1,n*cps,cps)], 1:2, is.nan) & jdf[,seq(1,n*cps,cps)]>0)
+
+    # column reordering makes it easy to recover af, dp, pv
+    jdf$jabc <- apply(jdf[,c(seq(1,n*cps,cps), seq(2,n*cps,cps), seq(3,n*cps,cps), seq(4,n*cps,cps))], 1,
+        function(row) {
+            afs <- row[1:8]
+            dps <- row[9:16]
+            pvs <- row[17:24]
+            sds <- row[25:32]
+            n.penalties <- sum(sds[dps>0 & afs>0] >= gp.sd.cutoff)
+            prod(pvs[dps>0 & afs>0]) * prod(rep(gp.sd.penalty, n.penalties))
+    })
+    jdf
+}
+
+# n - number of samples
+get.joint.cutoffs <- function(jdf, n, q=0.1) {
+    counts <- sapply(2:n, function(i) sum(jdf$ncells == i))
+    if (any(counts) == 0) {
+        cat("counts for ncells:\n")
+        for (i in 2:n)
+            cat(sprintf("ncells=%2d, count=%d\n", i, counts[i-1]))
+        stop("Found ncell count=0 (see above).  Try increasing the number of hSNP spikeins")
+    }
+
+    cutoffs <- sapply(2:n, function(i)
+        quantile(jdf$jabc[jdf$ncells == i], prob=q))
+    # ncells=0,1: don't pass any of these. they will be determined only by
+    # single sample statistics
+    data.frame(ncells=0:n, cutoff=c(Inf, Inf, cutoffs))
+}
+
+
+apply.joint.cutoffs <- function(jdf, cutoffs) {
+    mapply(function(ncells, jabc)
+        jabc >= cutoffs$cutoff[cutoffs$ncells == ncells],
+        ncells=jdf$ncells, jabc=jdf$jabc)
+}
